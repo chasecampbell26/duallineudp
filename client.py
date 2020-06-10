35f80933dd3ce952dc82ae194f9623a62f7a608b
Chase Campbell <56058674+chasecampbell26@users.noreply.github.com>
2020-06-10 00:32:08 -0700

    Fix client crash if primary_wan_socket receives first
    Also made constants and variable names more precise

--- client.py
+++ client.py
@@ -2,22 +2,23 @@ import socket
 import select
 import sys
 
-APP_IP = sys.argv[1]
-APP_PORT = int(sys.argv[2])
-PRIMARY_WAN_IP = sys.argv[3]
-SECONDARY_WAN_IP = sys.argv[4]
-WAN_PORT = int(sys.argv[5]) # local port, shared by primary and secondary WAN sockets
-SERVER_IP = sys.argv[6]
-SERVER_PORT = int(sys.argv[7])
+APP_LOCAL_IP = sys.argv[1] # should have been 127.0.0.1, but our app won't talk to it so listening on a trusted interface instead
+APP_LOCAL_PORT = int(sys.argv[2])
+PRIMARY_WAN_LOCAL_IP = sys.argv[3]
+# SECONDARY_WAN_LOCAL_IP = sys.argv[4]
+WAN_LOCAL_PORT = int(sys.argv[5]) # shared by primary and secondary WAN sockets
+WAN_REMOTE_IP = sys.argv[6]
+WAN_REMOTE_PORT = int(sys.argv[7])
 MAX_DATAGRAM_LENGTH = 4096
 
-socket.setdefaulttimeout(0)
+# We don't know this at the start. App knows our address, and we rely on them to tell us theirs by sending us the first datagram
+app_remote_address = None
 
 app_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-app_socket.bind((APP_IP, APP_PORT))
+app_socket.bind((APP_LOCAL_IP, APP_LOCAL_PORT))
 
 primary_wan_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-primary_wan_socket.bind((PRIMARY_WAN_IP, WAN_PORT))
+primary_wan_socket.bind((PRIMARY_WAN_LOCAL_IP, WAN_LOCAL_PORT))
 
 #secondary_wan_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 #secondary_wan_socket.bind((SECONDARY_WAN_IP, WAN_PORT))
@@ -31,25 +32,30 @@ while True:
 
     # TODO condense sent/received logging
     if app_socket in read:
-        (data, (app_ip, app_port)) = app_socket.recvfrom(MAX_DATAGRAM_LENGTH)
+        (data, app_read_address) = app_socket.recvfrom(MAX_DATAGRAM_LENGTH)
+        if app_read_address != app_remote_address:
+            app_remote_address = app_read_address
+            print("app remote address now changed to", app_read_address)
         if len(data) == MAX_DATAGRAM_LENGTH:
             raise OSError("Received max length datagram")
-        print("data received on app_socket from", app_ip, app_port)
 
-        len_sent = primary_wan_socket.sendto(bytes([1]) + data, (SERVER_IP, SERVER_PORT))
+        len_sent = primary_wan_socket.sendto(bytes([1]) + data, (WAN_REMOTE_IP, WAN_REMOTE_PORT))
         if len_sent < len(data) + 1:
             print("len_sent", len_sent, "data", len(data))
             raise OSError("len_sent not equal to data + 1")
-        print("data sent on primary_wan_socket")
 
     if primary_wan_socket in read:
-        (datagram, (server_ip, server_port)) = primary_wan_socket.recvfrom(MAX_DATAGRAM_LENGTH)
+        (datagram, primary_wan_read_address) = primary_wan_socket.recvfrom(MAX_DATAGRAM_LENGTH)
+        if primary_wan_read_address != (WAN_REMOTE_IP, WAN_REMOTE_PORT):
+            print("Received datagram on primary_wan_socket from unexpected address", primary_wan_read_address)
+            continue
         if len(datagram) == MAX_DATAGRAM_LENGTH:
             raise OSError("Received max length datagram")
-        print("data received on primary_wan_socket from", server_ip, server_port)
-        
-        len_sent = app_socket.sendto(datagram[1:], (app_ip, app_port))
+
+        if app_remote_address == None:
+            print("app_remote_address not set yet, dropping datagram")
+            continue
+        len_sent = app_socket.sendto(datagram[1:], app_remote_address)
         if len_sent < len(datagram) - 1:
             print("len_sent", len_sent, "datagram", len(datagram))
             raise OSError("len_sent not equal to datagram - 1")
-        print("data sent on app_socket to", app_ip, app_port)
