5ac2846726e7f7b0261ea629bbe4968176c8fbdf
Chase Campbell <56058674+chasecampbell26@users.noreply.github.com>
2020-06-28 01:14:12 -0700

    Client: select for write and retry if blocked on previous write

--- client.py
+++ client.py
@@ -25,72 +25,94 @@ primary_wan_socket.bind((PRIMARY_WAN_LOCAL_IP, WAN_LOCAL_PORT))
 #secondary_wan_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 #secondary_wan_socket.bind((SECONDARY_WAN_IP, WAN_PORT))
 
+app_to_primary_wan_pending = None
+primary_wan_to_app_pending = None
+
 sel = selectors.DefaultSelector()
 sel.register(app_socket, selectors.EVENT_READ)
 sel.register(primary_wan_socket, selectors.EVENT_READ)
 
+# returns True if program should block before next call
 def app_to_primary_wan():
+    global app_to_primary_wan_pending
     global app_remote_address
-    reads = 0
-    while reads < MAX_CONSECUTIVE_READS:
+    if not(app_to_primary_wan_pending):
         try:
             (datagram, app_read_address) = app_socket.recvfrom(MAX_DATAGRAM_LENGTH)
         except BlockingIOError:
-            return
-        reads += 1
+            return True
 
         if app_read_address != app_remote_address:
             app_remote_address = app_read_address
             print("app remote address now changed to", app_read_address)
         if len(datagram) == MAX_DATAGRAM_LENGTH:
             raise OSError("Received max length datagram")
+        app_to_primary_wan_pending = bytes([1]) + datagram
 
-        try:
-            len_sent = primary_wan_socket.sendto(bytes([1]) + datagram, (WAN_REMOTE_IP, WAN_REMOTE_PORT))
-        except BlockingIOError as bioe:
-            print("BlockingIOError on primary_wan_socket send")
-            if hasattr(bioe, "characters_written"):
-                print("Chars sent:", bioe.characters_written)
-            continue
-        if len_sent < len(datagram) + 1:
-            print("len_sent", len_sent, "len(datagram)", len(datagram))
-            raise OSError("len_sent not equal to len(datagram) + 1")
+    try:
+        len_sent = primary_wan_socket.sendto(app_to_primary_wan_pending, (WAN_REMOTE_IP, WAN_REMOTE_PORT))
+    except BlockingIOError as bioe:
+        return True
+    if len_sent < len(app_to_primary_wan_pending):
+        print("len_sent", len_sent, "app_to_primary_wan_pending", len(app_to_primary_wan_pending))
+        raise OSError("len_sent not equal to len(app_to_primary_wan_pending)")
+    app_to_primary_wan_pending = None
+    return False
 
+# returns True if program should block before next call
 def primary_wan_to_app():
-    reads = 0
-    while reads < MAX_CONSECUTIVE_READS:
+    global primary_wan_to_app_pending
+    if not(primary_wan_to_app_pending):
         try:
             (datagram, primary_wan_read_address) = primary_wan_socket.recvfrom(MAX_DATAGRAM_LENGTH)
         except BlockingIOError:
-            return
-        reads += 1
+            return True
 
         if primary_wan_read_address != (WAN_REMOTE_IP, WAN_REMOTE_PORT):
             print("Received datagram on primary_wan_socket from unexpected address", primary_wan_read_address)
-            continue
+            return False
         if len(datagram) == MAX_DATAGRAM_LENGTH:
             raise OSError("Received max length datagram")
 
         if app_remote_address == None:
             print("app_remote_address not set yet, dropping datagram")
-            continue
+            return False
+        primary_wan_to_app_pending = datagram[1:]
 
-        try:
-            len_sent = app_socket.sendto(datagram[1:], app_remote_address)
-        except BlockingIOError as bioe:
-            print("BlockingIOError on app_socket send")
-            if hasattr(bioe, "characters_written"):
-                print("Chars sent:", bioe.characters_written)
-            continue
-        if len_sent < len(datagram) - 1:
-            print("len_sent", len_sent, "datagram", len(datagram))
-            raise OSError("len_sent not equal to datagram - 1")
+    try:
+        len_sent = app_socket.sendto(primary_wan_to_app_pending, app_remote_address)
+    except BlockingIOError as bioe:
+        return True
+    if len_sent < len(primary_wan_to_app_pending):
+        print("len_sent", len_sent, "primary_wan_to_app_pending", len(primary_wan_to_app_pending))
+        raise OSError("len_sent not equal to primary_wan_to_app_pending")
+    primary_wan_to_app_pending = None
+    return False
 
 while True:
-    ready_sockets = sel.select()
-    for key, events in ready_sockets:
-        if key.fileobj == app_socket:
-            app_to_primary_wan()
+    # not inlining to avoid short-circuiting primary_wan_to_app
+    app_to_primary_wan_should_block = app_to_primary_wan()
+    primary_wan_to_app_should_block = primary_wan_to_app()
+    if app_to_primary_wan_should_block and primary_wan_to_app_should_block:
+        mapping = sel.get_map()
+        if app_socket in mapping:
+            sel.unregister(app_socket)
+        if primary_wan_socket in mapping:
+            sel.unregister(primary_wan_socket)
 
-        if key.fileobj == primary_wan_socket:
-            primary_wan_to_app()
+        app_socket_events = 0
+        primary_wan_socket_events = 0
+        if app_to_primary_wan_pending:
+            primary_wan_socket_events |= selectors.EVENT_WRITE
+        else:
+            app_socket_events |= selectors.EVENT_READ
+        if primary_wan_to_app_pending:
+            app_socket_events |= selectors.EVENT_WRITE
+        else:
+            primary_wan_socket_events |= selectors.EVENT_READ
+
+        if app_socket_events:
+            sel.register(app_socket, app_socket_events)
+        if primary_wan_socket_events:
+            sel.register(primary_wan_socket, primary_wan_socket_events)
+        sel.select()
